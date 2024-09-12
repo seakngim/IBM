5650ba31fe0811b013b4a890e2754792e28d9f89
SEAKNGIM <seakngimsr37@gmail.com>
2024-09-12 14:08:47 +0700

    first commit

--- src/main/java/com/data_presto/DataPrestoApplication.java
+++ src/main/java/com/data_presto/DataPrestoApplication.java
@@ -1,13 +1,35 @@
 package com.data_presto;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jdbc.JdbcRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-@SpringBootApplication
-public class DataPrestoApplication {
+import java.util.List;
+import java.util.Map;
 
-    public static void main(String[] args) {
-        SpringApplication.run(DataPrestoApplication.class, args);
-    }
+@SpringBootApplication(exclude = {JdbcRepositoriesAutoConfiguration.class, DataSourceAutoConfiguration.class})
+public class DataPrestoApplication implements CommandLineRunner {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataPrestoApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		// Query from Presto (replace 'your_table' with actual table name)
+		String sql = "SELECT * FROM \"iceberg_data\".\"preston_data\".\"tb_banking\"";
+
+		List<Map<String, Object>> results = jdbcTemplate.queryForList(sql);
+
+		// Process the result set
+		for (Map<String, Object> row : results) {
+			System.out.println(row);
+		}
+	}
 
 }
--- src/main/java/com/data_presto/PrestoConfig.java
+++ src/main/java/com/data_presto/PrestoConfig.java
@@ -1,2 +1,20 @@
-package com.data_presto;public class PrestoConfig {
+package com.data_presto;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PrestoConfig {
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName("com.facebook.presto.jdbc.PrestoDriver")
+                .url("jdbc:presto://dfc14c37-9185-4336-95ac-f734b6bb62cd.co1u05ot0jqujbai8j2g.lakehouse.appdomain.cloud:31384/iceberg_data?SSL=true")
+                .username("ibmlhapikey")
+                .password("8BTw1wttn0Vmcs8t8tRQailLGyAfYSs8Nl0aABQ4Y-i3")
+                .build();
+    }
 }
